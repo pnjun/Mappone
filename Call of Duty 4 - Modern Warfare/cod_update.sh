ea2933973f5457dc6a35f719a77d650a73f31d1f
wolf 32d <elia.mulas@gmail.com>
2012-03-13 16:22:58 +0100

    script

--- Call of Duty 4 - Modern Warfare/cod_update.sh
+++ Call of Duty 4 - Modern Warfare/cod_update.sh
@@ -3,3 +3,5 @@ git add raw/materials/uni_*
 git add source_data/*
 git add map_source/prefabs/aa_mappa_cod/*
 git add map_source/aa_mappa_uni.map
+git add texture_uni/*
+git add model_export/uni_modelli/*
